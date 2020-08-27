b31bff814c5711471612602f3b27b25ca29e704f
kokon322 <chornuy322@gmail.com>
2020-08-27 16:32:55 +0200

    new Chenge

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
   <h1>ToDoList</h1>  
   <input type="text" v-model="scaner.scan">
   <button @click="addToData">addToData</button>
-
+  <button @click="getFromData">GetFromData</button>
+  <ul>
+    <li v-for="(todo , i) in toDoList" :key="i">
+        {{todo.scan}}
+    </li>
+  </ul>
 
   </div>
 </template>
@@ -25,7 +30,19 @@ export default {
   methods:{
   addToData(){
     this.$http.post('https://lesson----vue.firebaseio.com/todo.json', this.scaner)
-    
+    this.scaner.scan = ''
+    },
+  getFromData(){
+    this.$http.get('https://lesson----vue.firebaseio.com/todo.json')
+    .then((res)=>{
+      return res.json()
+    }).then((res) =>{
+       return Object.values(res)    
+    }).then((res)=>{
+      for(let item of res){
+        this.toDoList.push(item)
+      }
+    })
   }
     
   }
